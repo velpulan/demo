134d3332829cb6be4e5f838b74eda02ecf0fa91a
root <root@ip-172-31-32-111.ap-south-1.compute.internal>
2023-03-15 10:03:14 +0000

    wedproject

---
+++ src/main/java/com/hellov/App.java
@@ -0,0 +1,13 @@
+package com.hellov;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
